033e8b5264777c875f3fa745f6b3a975b63b8be0
Wisnu Andika <wisnuandika104@gmail.com>
2024-02-15 22:14:19 +0700

    Initial commit

---
+++ app/src/main/java/com/dev/modul2a/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dev.modul2a;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
